452321926ad1f0f89a1c7d96ab77f2fca86a55db
Maayan21 <76556838+Maayan21@users.noreply.github.com>
2020-12-27 15:24:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -63,13 +63,25 @@ score_value = 0
 label="00:02:00"
 
 font = pygame.font.Font('freesansbold.ttf', 32)
+game4=False
+# Colors
+white=(255, 255, 255)
+black=(0, 0, 0)
+gray=(50, 50, 50)
+red=(255, 0, 0)
+green=(0, 255, 0)
+blue=(0, 0, 255)
+yellow=(255, 255, 0)
 
+# Game Fonts
+font1 = "Retro.ttf"
 textX = 10
 testY = 10
 
 # Game Over
 over_font = pygame.font.Font('freesansbold.ttf', 64)
-count = 7000
+
+count=7000
 flag=True
 
 # define the countdown func.
@@ -124,6 +136,9 @@ def fire_bullet(x, y):
     screen.blit(bulletImg, (x + 16, y + 10))
 
 
+def textObject(text, font):
+    textSurface = font.render(text,True,(255,255,255))
+    return textSurface, textSurface.get_rect()
 def isCollision(enemyX, enemyY, bulletX, bulletY):
     distance = math.sqrt(math.pow(enemyX - bulletX, 2) + (math.pow(enemyY - bulletY, 2)))
     if distance < 27:
@@ -131,176 +146,179 @@ def isCollision(enemyX, enemyY, bulletX, bulletY):
     else:
         return False
 
+def button(msg,x,y,w,h,ic,ac,):
+    mouse = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed()
 
-# Game Loop
-running = True
-while running:
-
-    # RGB = Red, Green, Blue
-    screen.fill((0, 0, 0))
-    # Background Image
-    screen.blit(background, (0, 0))
-
-    if flag == True:
-        count -= 1
-        clock.tick(100)
+    if x + w > mouse[0] > x and y + h > mouse[1] > y:
+        pygame.draw.rect(screen, ac, (x, y, w, h))
+    else:
+        pygame.draw.rect(screen, ic, (x, y, w, h))
+    button_8 = pygame.Rect(x, y, w, h)
 
 
+    smallText = pygame.font.Font("Retro.ttf", 50)
+    textSurf, textRect = textObject(msg, smallText)
+    textRect.center = ((x + (w / 2)), (y + (h / 2)))
+    screen.blit(textSurf, textRect)
+    return button_8
+click = False
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+def game():
+    global playerX,playerX_change,bulletY,bulletX,bulletX_change,bullet_state,bulletY_change,game4
+    game4=True
+    running = True
+    while running:
+        screen.fill((0, 0, 0))
+        screen.blit(background, (0, 0))
+        draw_text('game', font, (255, 255, 255), screen, 20, 20)
+        global flag
+        if flag == True:
+            global count
+            count -=1
+            clock.tick(100)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
 
-        # if keystroke is pressed check whether its right or left
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                playerX_change = -5
-            if event.key == pygame.K_RIGHT:
-                playerX_change = 5
-            if event.key == pygame.K_SPACE:
-                if bullet_state is "ready":
-                    bulletSound = mixer.Sound("laser.wav")
-                    bulletSound.play()
-                    bulletX = playerX
-                    fire_bullet(bulletX, bulletY)
+            # if keystroke is pressed check whether its right or left
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    playerX_change = -5
+                if event.key == pygame.K_RIGHT:
+                    playerX_change = 5
+                if event.key == pygame.K_SPACE:
+                    if bullet_state is "ready":
+                        bulletSound = mixer.Sound("laser.wav")
+                        bulletSound.play()
+                        bulletX = playerX
+                        fire_bullet(bulletX, bulletY)
 
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                playerX_change = 0
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                    playerX_change = 0
 
-    # 5 = 5 + -0.1 -> 5 = 5 - 0.1
-    # 5 = 5 + 0.1
+        playerX += playerX_change
+        if playerX <= 0:
+            playerX = 0
+        elif playerX >= 736:
+            playerX = 736
 
-    playerX += playerX_change
-    if playerX <= 0:
-        playerX = 0
-    elif playerX >= 736:
-        playerX = 736
+        # Enemy Movement
+        for i in range(num_of_enemies):
 
-    # Enemy Movement
-    for i in range(num_of_enemies):
+            # Game Over
+            if enemyY[i] > 440 or count <= 0:
+                for j in range(num_of_enemies):
+                    flag = False
+                    count = 0
+                    enemyY[j] = 2000
+                game_over_text()
+                break
 
-        # Game Over
-        if enemyY[i] > 440 or count<=0:
-            for j in range(num_of_enemies):
-                flag=False
-                count=0
-                enemyY[j] = 2000
-            game_over_text()
-            break
+            enemyX[i] += enemyX_change[i]
+            if enemyX[i] <= 0:
+                enemyX_change[i] = 4
+                enemyY[i] += enemyY_change[i]
+            elif enemyX[i] >= 736:
+                enemyX_change[i] = -4
+                enemyY[i] += enemyY_change[i]
 
-        enemyX[i] += enemyX_change[i]
-        if enemyX[i] <= 0:
-            enemyX_change[i] = 4
-            enemyY[i] += enemyY_change[i]
-        elif enemyX[i] >= 736:
-            enemyX_change[i] = -4
-            enemyY[i] += enemyY_change[i]
+            # Collision
+            collision = isCollision(enemyX[i], enemyY[i], bulletX, bulletY)
+            if collision:
+                explosionSound = mixer.Sound("explosion.wav")
+                explosionSound.play()
+                bulletY = 480
+                bullet_state = "ready"
+                global score_value
+                score_value += 1
+                enemyX[i] = random.randint(0, 736)
+                enemyY[i] = random.randint(50, 150)
 
-        # Collision
-        collision = isCollision(enemyX[i], enemyY[i], bulletX, bulletY)
-        if collision:
-            explosionSound = mixer.Sound("explosion.wav")
-            explosionSound.play()
+            enemy(enemyX[i], enemyY[i], i)
+
+        # Bullet Movement
+        if bulletY <= 0:
             bulletY = 480
             bullet_state = "ready"
-            score_value += 1
-            enemyX[i] = random.randint(0, 736)
-            enemyY[i] = random.randint(50, 150)
 
-        enemy(enemyX[i], enemyY[i], i)
+        if bullet_state is "fire":
+            fire_bullet(bulletX, bulletY)
+            bulletY -= bulletY_change
 
-    # Bullet Movement
-    if bulletY <= 0:
-        bulletY = 480
-        bullet_state = "ready"
-
-    if bullet_state is "fire":
-        fire_bullet(bulletX, bulletY)
-        bulletY -= bulletY_change
+        player(playerX, playerY)
+        show_score(textX, testY)
+        pygame.display.update()
+        mainClock.tick(60)
 
 
-    def textObject(text, font):
-        textSurface = font.render(text,True,(255,255,255))
-        return textSurface, textSurface.get_rect()
+def QuitGame():
+    pygame.quit()
+    quit()
+def options():
+
+    running = True
+    while running:
+        screen.fill((0, 0, 0))
+
+        draw_text('options', font, (255, 255, 255), screen, 20, 20)
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    running = False
+
+        pygame.display.update()
+        mainClock.tick(60)
+def draw_text(text, font, color, surface, x, y):
+    textobj = font.render(text, 1, color)
+    textrect = textobj.get_rect()
+    textrect.topleft = (x, y)
+    surface.blit(textobj, textrect)
+def text_format(message, textFont, textSize, textColor):
+    newFont=pygame.font.Font(textFont, textSize)
+    newText=newFont.render(message, 0, textColor)
+
+    return newText
+def main_menu():
+    while True:
+        screen.fill(blue)
+        title = text_format("main menu", font1, 90, yellow)
+        title_rect = title.get_rect()
+        screen.blit(title, (800 / 2 - (title_rect[2] / 2), 80))
+        mx, my = pygame.mouse.get_pos()
+        if game4==True:
+            if button("continue", 300, 200, 200, 50, blue, yellow).collidepoint((mx, my)):
+                if click:
+                    game()
+        elif button("new game", 300, 200, 200, 50, blue, yellow).collidepoint((mx, my)):
+            if click:
+                game()
+        if button("Quit", 300, 400, 200, 50, blue, yellow).collidepoint((mx, my)):
+            if click:
+                QuitGame()
 
 
-    def draw_text(text, font, color, surface, x, y):
-        textobj = font.render(text, 1, color)
-        textrect = textobj.get_rect()
-        textrect.topleft = (x, y)
-        surface.blit(textobj, textrect)
-
-    def button(msg,x,y,w,h,ic,ac,):
-        mouse = pygame.mouse.get_pos()
-        click = pygame.mouse.get_pressed()
-
-        if x + w > mouse[0] > x and y + h > mouse[1] > y:
-            pygame.draw.rect(screen, ac, (x, y, w, h))
-        else:
-            pygame.draw.rect(screen, ic, (x, y, w, h))
-        button_8 = pygame.Rect(x, y, w, h)
-
-
-        smallText = pygame.font.Font("freesansbold.ttf", 20)
-        textSurf, textRect = textObject(msg, smallText)
-        textRect.center = ((x + (w / 2)), (y + (h / 2)))
-        screen.blit(textSurf, textRect)
-        return button_8
-
-    click = False
-
-
-    def options():
-        running = True
-        while running:
-            screen.fill((0, 0, 0))
-
-            draw_text('options', font, (255, 255, 255), screen, 20, 20)
-            for event in pygame.event.get():
-                if event.type == QUIT:
+        if  button("options", 300, 300, 200, 50, blue, yellow).collidepoint((mx, my)):
+            if click:
+                options()
+        click = False
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
                     pygame.quit()
                     sys.exit()
-                if event.type == KEYDOWN:
-                    if event.key == K_ESCAPE:
-                        running = False
+            if event.type == MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    click = True
 
-            pygame.display.update()
-            mainClock.tick(60)
+        pygame.display.update()
+        mainClock.tick(60)
 
-
-    def main_menu():
-        while True:
-
-            screen.fill((0, 0, 0))
-            draw_text('main menu', font, (255, 255, 255), screen, 20, 20)
-
-            mx, my = pygame.mouse.get_pos()
-            if  button("Go", 150, 450, 100, 50, (255, 0, 0), (255, 0, 0)).collidepoint((mx, my)):
-                if click:
-                    options()
-
-            if  button("Quit", 550, 450, 100, 50, (255, 255, 0), (255, 255, 0)).collidepoint((mx, my)):
-                if click:
-                    options()
-            click = False
-            for event in pygame.event.get():
-                if event.type == QUIT:
-                    pygame.quit()
-                    sys.exit()
-                if event.type == KEYDOWN:
-                    if event.key == K_ESCAPE:
-                        pygame.quit()
-                        sys.exit()
-                if event.type == MOUSEBUTTONDOWN:
-                    if event.button == 1:
-                        click = True
-
-            pygame.display.update()
-            mainClock.tick(60)
-
-    player(playerX, playerY)
-    show_score(textX, testY)
-    pygame.display.update()
-
-
+main_menu()
